2187c6a2813213fb19cc74b467b18b1a3e49882c
rashidsarkar <rashidrock558@gmail.com>
2023-10-04 11:56:19 +0600

    react

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCib7l4u5EnvPGNGfWPBaIH3dx1KLqBdP4",
+  authDomain: "react-dragon-news-edfc4.firebaseapp.com",
+  projectId: "react-dragon-news-edfc4",
+  storageBucket: "react-dragon-news-edfc4.appspot.com",
+  messagingSenderId: "414944514394",
+  appId: "1:414944514394:web:a64af43d7e61e435cd2363",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <div>
+      <h2 className="text-3x">tjis is login</h2>
+    </div>
+  );
+}
+
+export default Login;
